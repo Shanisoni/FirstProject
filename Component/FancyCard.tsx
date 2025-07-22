65cb840a655b32d88d62bb23e7992f6e41d057b1
Shani Soni <134639145+Shanisoni@users.noreply.github.com>
2025-07-22 22:09:38 +0530

    commited

--- Component/FancyCard.tsx
+++ Component/FancyCard.tsx
@@ -13,8 +13,16 @@ export default function FancyCard() {
           }}
           style={{ width: 300, height: 200, borderRadius: 10 }}
         />
+        <View style={styles.cardBody}>
+            <Text style={styles.cardTitle}>Beautiful Beach</Text>
+            <Text style={styles.cardLable}>Pink City, Jiapur</Text>
+            <Text style={styles.cardDescription}>
+              A serene beach with golden sands and clear blue waters.
+            </Text>
+            <Text style={styles.cardFooter}> 12 minutes away</Text>
+          </View>
+        </View>
       </View>
-    </View>
   );
 }
 
@@ -25,7 +33,44 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 20,
   },
-  cardElevated: {},
+  cardElevated: {
+    margin: 10,
+    // elevation: 5,
+    // shadowColor: 'black',
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.3,
+    // shadowRadius: 4,
+  },
 
-  card: {},
+  card: {
+    height: 300,
+    width: 350,
+    borderRadius: 10,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    overflow: 'hidden',
+  },
+    cardBody: {
+        padding: 10,
+    },
+    cardTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    cardLable: {
+        fontSize: 16,
+        color: 'gray',
+    },
+
+    cardDescription: {
+        fontSize: 14,
+        color: 'black',
+        marginVertical: 5,
+    },
+    cardFooter: {
+        fontSize: 120,
+        color: 'blue',
+        // marginTop: 10,
+    },
+
 });
