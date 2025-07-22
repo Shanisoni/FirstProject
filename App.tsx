64b17ebb45b618953d71e27ee48b1ca2d15a9d70
Shani Soni <134639145+Shanisoni@users.noreply.github.com>
2025-07-22 15:53:03 +0530

    c

--- App.tsx
+++ App.tsx
@@ -7,7 +7,12 @@
 
 // import { NewAppScreen } from '@react-native/new-app-screen';
 import {  StyleSheet, useColorScheme, View } from 'react-native';
-import { Text } from 'react-native';
+import { 
+  Text,
+  SafeAreaView,
+  ScrollView,
+
+ } from 'react-native';
 import React from 'react';
 
 
@@ -16,7 +21,15 @@ function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
+    <SafeAreaView>
+      <ScrollView>
+        
+      </ScrollView>
+
+
+    </SafeAreaView>
    
+  );
 }
 
 const styles = StyleSheet.create({
