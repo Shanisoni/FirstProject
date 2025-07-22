888b0bd05b6cdd13a9e012a913fa477cf143f11a
Shani Soni <134639145+Shanisoni@users.noreply.github.com>
2025-07-22 15:57:11 +0530

    commited

--- App.tsx
+++ App.tsx
@@ -6,36 +6,35 @@
  */
 
 // import { NewAppScreen } from '@react-native/new-app-screen';
-import {  StyleSheet, useColorScheme, View } from 'react-native';
-import { 
-  Text,
+import {  
+  // StyleSheet,
   SafeAreaView,
   ScrollView,
+  Text,
+  //  useColorScheme,
+  } from 'react-native';
 
- } from 'react-native';
 import React from 'react';
 
 
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  // const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <SafeAreaView>
       <ScrollView>
-        
+        <Text> Shani Soni</Text>
       </ScrollView>
-
-
     </SafeAreaView>
    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
 
 export default App;
